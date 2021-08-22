09f56fc995ed4f19cf995fc0856e6fc5e6a46e1d
Archit Soni <archit2@ualberta.ca>
2021-08-22 17:52:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,84 @@
-from flask import Flask
+from flask import Flask, send_file, request, jsonify
+#from dotenv import load_dotenv          # Ask Jason for the dotenv file details!
 import os
+from flask.json import JSONEncoder, jsonify
+import requests
+import shutil                           # Used to save the photos locally
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\archi\\Documents\\GitHub\\inventory\\Inventory\\Inventory-43cc8f010091.json'
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': "inventory-1629511801394",
+})
+
+db = firestore.client()
+
+#load_dotenv()
 
 app = Flask(__name__)
+
+# Configuration for spoonacular API
+API_KEY = os.getenv("API_KEY")
+SPOON_BASEURL = os.getenv("SPOON_BASEURL")
+SPOON_IMGURL = os.getenv("SPOON_IMGURL")
+IMAGE_SIZE = "250x250"
+    
 
 
 @app.route("/")
 def index():
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\archi\\Documents\\GitHub\\inventory\\Inventory\\Inventory-43cc8f010091.json'
-    scrapURL("https://www.allrecipes.com/recipe/17481/simple-white-cake/")
-    textFromPath(
-        "C:\\Users\\archi\\Documents\\GitHub\\inventory\\Inventory\\Untitled.png")
-    textFromURI("https://i.imgur.com/pCkrQky.png")
-    textFromURI(
-        "http://sunnymoney.weebly.com/uploads/1/9/6/4/19645963/veggie-grocery-receipt_orig.jpeg")
+    #scrapURL("https://www.allrecipes.com/recipe/17481/simple-white-cake/")
+    #textFromPath(
+    #     "C:\\Users\\archi\\Documents\\GitHub\\inventory\\Inventory\\Untitled.png")
+    #textFromURI("https://i.imgur.com/pCkrQky.png")
+    #textFromURI(
+    #     "http://sunnymoney.weebly.com/uploads/1/9/6/4/19645963/veggie-grocery-receipt_orig.jpeg")
     return "Hello"
 
+@app.route("/api/<ingredient_name>")
+def get_ingredient(ingredient_name):
+    '''Query Spoonacular API using requests and Display an Image of the Ingredient'''
+
+    # Parameters we want to provide in our ingredient search
+    payload = {
+        "query": ingredient_name,
+        "number": 1,
+        "apiKey": API_KEY,
+    }
+    try: 
+        INGREDIENT_URL = f"{SPOON_BASEURL}/food/ingredients/search"
+        response = requests.get(INGREDIENT_URL, params=payload).json()
+
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+        return f"There was an error querying Spoonacular's API for {ingredient_name}!" 
+    
+    # Create a URL that represents the ingredient image 
+    image_route = f"{SPOON_IMGURL}{IMAGE_SIZE}/{response['results'][0]['image']}"
+    try:
+        '''Heads to the route of the ingredient and saves it locally'''
+        img_response = requests.get(image_route, stream = True)
+        if img_response.status_code == 200:
+            img_response.raw.decode_content = True
+
+            # Make sure you have a photos directory or this will crash!
+            filename = f"./photos/{ingredient_name}.jpg"
+            with open(filename, "wb") as f:
+                '''Saves the image to your local directory'''
+                shutil.copyfileobj(img_response.raw, f)
+
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+        return f"There was an error trying to get the image for a {ingredient_name}"
+
+    return send_file(filename, mimetype="image")
+    
 
 def scrapURL(url):
     import requests
@@ -30,23 +93,27 @@ def scrapURL(url):
     return ings
 
 
-def textFromURI(uri):
+@app.route("/uri/")
+def textFromURI():
+    uri = request.args.get('uri')
     """Detects text in the file located in Google Cloud Storage or on the Web.
     """
     from google.cloud import vision
     client = vision.ImageAnnotatorClient()
     image = vision.Image()
     image.source.image_uri = uri
-
     response = client.text_detection(image=image)
     texts = response.text_annotations
+    data = {}
+    data["ings"] = []
+    data["filter"] = []
     print('Texts:')
-
     for text in texts:
         print('\n"{}"'.format(text.description))
+        data["ings"].append(text.description)
 
         vertices = (['({},{})'.format(vertex.x, vertex.y)
-                     for vertex in text.bounding_poly.vertices])
+                    for vertex in text.bounding_poly.vertices])
 
         print('bounds: {}'.format(','.join(vertices)))
 
@@ -55,8 +122,15 @@ def textFromURI(uri):
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
-    print(texts)
-    return texts
+    print()
+    splt = data['ings'][0].split('\n')
+    for ing in splt:
+        if '$' not in ing:
+            if len(ing)>5:
+                if '.' not in ing:
+                    data['filter'].append(ing)
+    print(data['filter'])
+    return jsonify(data)
 
 
 def textFromPath(path):
@@ -79,8 +153,7 @@ def textFromPath(path):
         print('\n"{}"'.format(text.description))
 
         vertices = (['({},{})'.format(vertex.x, vertex.y)
-                     for vertex in text.bounding_poly.vertices])
-
+                    for vertex in text.bounding_poly.vertices])
         print('bounds: {}'.format(','.join(vertices)))
 
     if response.error.message:
